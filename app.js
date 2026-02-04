48a15b2bd511a43bb70dec250efa8e129a073f1c
mdz801 <migueldavidmartinezpetit@gmail.com>
2026-02-04 16:03:05 -0500

    vomic

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', linkRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     const url = `http://localhost:${PORT}`;
